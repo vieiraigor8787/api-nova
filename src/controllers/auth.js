998fead40c1fb5970a22807b8c11dfc162d80859
Igor Cerqueira <igor_vc@hotmail.com>
2019-08-18 12:51:05 -0300

    ajustando password

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -5,8 +5,15 @@ const User = require('../models/user')
 const router = express.Router()
 
 router.post('/register', async (req, res) => {
+    const { email }  = req.body
     try {
+        if (await User.findOne({ email }))
+        return res.status(400).send({ error: 'Usuário ja existente' })
+
         const user = await User.create(req.body)
+
+        user.password = undefined
+
         return res.send({ user })    
     } catch (err) {
         return res.status(400).send({ error: 'Falha no registro' })
--- src/database/index.js
+++ src/database/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 //faltou vc passar a por 27017 que é padrão do mongodb
 const uri = 'mongodb://localhost:27017/noderest'
-mongoose.createConnection(uri, { useNewUrlParser: true })
+mongoose.connect(uri, { useNewUrlParser: true })
 mongoose.Promise = global.Promise
 
 module.exports = mongoose
